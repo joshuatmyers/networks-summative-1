beca5b9146899f434b1050730acf2ba94ccc01ea
Josh <joshuamyers1097@gmail.com>
2023-12-03 16:42:04 +0000

    working with individual client

--- client.py
+++ client.py
@@ -1,20 +1,63 @@
 import socket
+import threading
+import sys
 
-def start_client():
-    serverName = "127.0.0.1"
-    serverPort = 8800
-    # initialise TCP socket
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# include strict typing
+class Client():
 
-    message = input("Input lowercase sentence: ")
+    def __init__(self, username):
+        # client ip and port
+        self.name = username
 
-    # the socket address is the tuple that contains the ip and port
-    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    # include threads to allow for more connections
+    # add message to confirm connection with server
+    
+    # create a new thread that just listens for messages from other users
+    def connect(self, ip, port):
 
-    # this line allows the client to receive messages from the server, saving the message and address from the server
-    modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-    print(modifiedMessage.decode())
-    clientSocket.close()
+        # server ip and port  
+        serverName = ip
+        serverPort = port
+
+        # initialise TCP socket
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # connect to the server
+        clientSocket.connect((serverName, serverPort))
+
+        # start listening immediately - messages from other users
+        #listen_thread = threading.Thread(target=self.listen, args=(clientSocket,))
+        #listen_thread.start()
+
+        while True:
+            message = input("Message to send: ")
+
+            if message == "close":
+                message = ""
+                clientSocket.send(message.encode())
+                clientSocket.close()
+                break
+
+            message = f'{self.name} : ' + message
+
+            # the socket address is the tuple that contains the ip and port
+            clientSocket.send(message.encode())
+
+            # this line allows the client to receive messages from the server, saving the message and address from the server
+            data, serverAddress = clientSocket.recvfrom(1024)
+            modifiedMessage = data.decode()
+            print(modifiedMessage)
+    
+    def listen(self, connectionSocket):
+        while True:
+            data, serverAddress = connectionSocket.recvfrom(1024)
+            modifiedMessage = data.decode()
+            print(modifiedMessage)
+        
 
 if __name__ == "__main__":
-    start_client()
+    # python client.py "username" "hostname" "port"
+    # add some checks with RE's to validate format
+    client = Client(sys.argv[1])
+    client.connect(str(sys.argv[2]), int(sys.argv[3]))
+
--- server.py
+++ server.py
@@ -1,21 +1,59 @@
 import socket
+import sys
+import threading
 
-def start_server():
-    # server listens to port 8800 for client to connect to
-    serverPort = 8800
-    # initialise a TCP socket - TCP is defined with 'SOCK_STREAM'
-    # AF_INET refers to the ipv4 address
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+# implement a keybind to shutdown the server in the future
+class Server():
     
-    
-    serverSocket.bind(("", serverPort))
-    print("The server is ready to receive")
-    while True: 
-        message, clientAddress = serverSocket.recvfrom(1024)
-        # modifies the message to be all uppercase
-        modifiedMessage = message.decode().upper() 
-        # send the message back to the client
-        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # include strict types for inputs
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.connections = {}
+
+    def start(self):
+        # initialise TCP socket
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        serverSocket.bind(("", self.port))
+        serverSocket.listen(5)
+        print("The server is ready to receive")
+        # keep looking for connection, then create new thread
+        while True:
+            connectionSocket, clientAddress = serverSocket.accept()
+            client_handler = threading.Thread(target=self.handle_client, args=(connectionSocket, clientAddress))
+            client_handler.start()
+        ## add message to confirm connection between server and client
+        
+
+    def handle_client(self, connectionSocket, clientAddress):
+        # Display the username rather than the ip address
+        print(f'{clientAddress} has connected to the server')
+        #print(connectionSocket)
+        self.connections[connectionSocket] = clientAddress
+        # send a message to show connection from client
+        while True:
+            message = connectionSocket.recv(1024).decode()
+            if not message:
+                break
+
+            print(message)
+            # format the message 
+            modifiedMessage = (f'{clientAddress[0]}: {message}')
+                
+            # send the message back to the all clients
+            for connection in self.connections.keys():
+                connection.send(modifiedMessage.encode())
+            #connectionSocket.send(modifiedMessage.encode())
+        
+        # close the socket and remove the client from the list of connections
+        connectionSocket.close()
+        print(f'{clientAddress} has disconnected from the server')
+        self.connections.pop(clientAddress[0])
+
+# implement a function that handles commands, e.g. /msg <username> <message> 
 
 if __name__=="__main__":
-    start_server()
+    #start_server()
+    # create server instance, listening on port 8800
+    server = Server('', int(sys.argv[1]))
+    server.start()
